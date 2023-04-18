af21ce5275c6690a64c532ce30b748b9799fc190
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-04-18 11:38:34 +0100

    file cleanup

---
+++ pages/add-hero/index.js
@@ -0,0 +1,9 @@
+const {
+  default: AddHeroForm,
+} = require("@/components/add-hero-form/add-hero-form");
+
+const AddHeroPage = () => {
+  return <AddHeroForm />;
+};
+
+export default AddHeroPage;
