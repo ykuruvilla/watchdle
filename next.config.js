3d5ff9a9517a92e48ac6085fe1d53ab120cd883a
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-04-18 12:37:09 +0100

    setup env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "yashaKuruvilla",
+        mongodb_password: "9xQ6qFn9HPp7FqL1",
+        mongodb_dbname: "watchdle",
+        mongodb_clustername: "heroes",
+      },
+    };
+  }
+};
