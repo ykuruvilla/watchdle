5db48d1f85a2e4e3c11381c2bc834b6febf4686f
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-04-24 01:23:36 +0100

    quick fix

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ const Home = () => {
     <>
       <Head>
         <title>Watchdle</title>
-        <meta name="description" content="An Overwatch game based on Worlde" />
+        <meta name="description" content="An Overwatch game based on Wordle" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
